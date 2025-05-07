f4c660e8b876ac8707e160f91c4cd53c5fa3f031
MyNameIsSyukra <sukrawahyu@gmail.com>
2025-05-07 11:57:15 +0700

    feat:fix docker

--- config/db.go
+++ config/db.go
@@ -15,7 +15,6 @@ func SetUpDatabaseConnection() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
-
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
@@ -23,10 +22,15 @@ func SetUpDatabaseConnection() *gorm.DB {
 	dbPort := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", dbHost, dbUser, dbPass, dbName, dbPort)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        panic("failed to connect database")
-    }
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
 
 	return db
 }
